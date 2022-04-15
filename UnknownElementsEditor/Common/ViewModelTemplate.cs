40cce18752ccafe5f66aab1a04f7eb54cbe943ea
DanielWhylie <danielwhylie@gmail.com>
2022-04-15 11:42:27 +0100

    Added text boxes in create project view that tracks whatever is entered into it. The project path text box contains the running system's MyDocument folder location

---
+++ UnknownElementsEditor/Common/ViewModelTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnknownElementsEditor
+{
+    class ViewModelTemplate : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ UnknownElementsEditor/GameProject/NewProject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnknownElementsEditor.GameProject
+{
+    class NewProject : ViewModelTemplate
+    {
+        private string _projectName = "NewProject";
+        public string Name
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\UnknownElements";
+        public string Path
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+}
